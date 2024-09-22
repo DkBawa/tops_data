132401ad8189280e4fa0033406a1f0fbd07cd166
DkBawa <dineshbaghana>
2024-09-22 11:45:40 +0530

    file2

--- Assignment/SE basic/module-3-SE/Looping and condition Statements/factorilans5.c
+++ Assignment/SE basic/module-3-SE/Looping and condition Statements/factorilans5.c
@@ -1,16 +1,17 @@
 // 5. WAP to print factorial of given number //
 #include<stdio.h>
 int main (){
-	int i,num,fact=1;
+	int i,num,fact=1,ft=0;
 	
 	printf("Enter a num:",num);
 	scanf("%d",&num);
 	
 	for(i=1;i<=num; i++)
 	{
-		 fact = i*fact;   
+		 fact = i*fact;
+		 ft=fact;   
     }
-	printf("%d ka factorial is %d\n",num ,fact);  
+	printf("%d ka factorial is %d\n",num,ft);  
 	return 0;
 }
  
